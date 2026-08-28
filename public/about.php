2e7124f15df82030868e4725cd1fcf1029a3e2ef
Artemis <69371778+fokstreet@users.noreply.github.com>
2026-08-29 02:24:38 +0300

    add: php

---
+++ public/about.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>О компании - Aliance Production</title>
+</head>
+
+<body>
+
+    <div class="mobile-menu">
+        <div class="mobile-menu-nav">
+            <ul class="mobile-menu-nav">
+                <li class="mobile-menu-nav-item"><a href="./about.php" class="mobile-menu-link">О компании</a></li>
+                <li class="mobile-menu-nav-item"><a href="#" class="mobile-menu-link">Контрактное производство</a>
+                    <ul class="mobile-submenu">
+                        <li class="mobile-submenu-item">
+                            <a class="mobile-submenu-link" href="#">Автомобильная химия</a>
+                        </li>
+                        <li class="mobile-submenu-item">
+                            <a class="mobile-submenu-link" href="#">Бытовая химия</a>
+                        </li>
+                        <li class="mobile-submenu-item">
+                            <a class="mobile-submenu-link" href="#">Дезенфицирующие средства</a>
+                        </li>
+                    </ul>
+
+                </li>
+                <li class="mobile-menu-nav-item"><a href="#" class="mobile-menu-link">Собственные торговые марки</a>
+                </li>
+                <li class="mobile-menu-nav-item"><a href="#" class="mobile-menu-link">Новости</a></li>
+                <li class="mobile-menu-nav-item"><a href="#" class="mobile-menu-link">Контакты</a></li>
+            </ul>
+            <a href="tel:+74996861014" class="mobile-phone">+7 (499) 686-10-14</a>
+            <div class="mobile-info">
+                <svg class="phone-icon" width="24px" height="24px">
+                    <use href="img/sprite.svg#adress"></use>
+                </svg>
+                <address class="mobile-info-address"> г. Мосвка, Холодильный пер. 4к1с8 </address>
+            </div>
+            <div class="mobile-info">
+                <svg class="phone-icon" width="24px" height="24px">
+                    <use href="img/sprite.svg#mail"></use>
+                </svg>
+                <a href="mailto:a.dragunov@tdaliance.ru" class="mobile-info-email"> a.dragunov@tdaliance.ru </a>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-light">
+        <a href="" class="mobile-menu-toggle">
+            <div class="mobile-menu-line"></div>
+            <div class="mobile-menu-line"></div>
+            <div class="mobile-menu-line"></div>
+        </a>
+        <a href="./" class="header-logo">
+            <svg class="logo-svg logo-light">
+                <use href="img/sprite.svg#logo-light"></use>
+            </svg>
+
+            <svg class="logo-svg logo-dark">
+                <use href="img/sprite.svg#logo"></use>
+            </svg>
+
+        </a>
+        <ul class="header-nav">
+            <li class="header-nav-item"><a href="./about.php" class="header-nav-link">О компании</a></li>
+            <li class="header-nav-item"><a href="#" class="header-nav-link">Контрактное производство</a></li>
+            <li class="header-nav-item"><a href="#" class="header-nav-link">Собственные торговые марки</a></li>
+            <li class="header-nav-item"><a href="#" class="header-nav-link">Новости</a></li>
+            <li class="header-nav-item"><a href="#" class="header-nav-link">Контакты</a></li>
+        </ul>
+
+        <div class="header-phone">
+            <svg class="phone-icon" width="24px" height="24px">
+                <use href="img/sprite.svg#phone"></use>
+            </svg>
+            <a href="tel:+74996861014" class="header-phone-link">
+                +7 (499) 686-10-14
+            </a>
+        </div>
+        <button class="navbar-button button" data-toggle="modal">
+            <svg class="button-icon" width="24px" height="24px">
+                <use href="img/sprite.svg#phone"></use>
+            </svg>
+            <span class="button-text">Получить консультацию</span>
+        </button>
+        <!-- /.header-phone -->
+    </nav>
+        <header class="header header-image">
+
+        <!-- /.navbar -->
+        <div class="container">
+            <div class="header-content">
+                <div class="seporator"></div>
+                <h1 class="header-title">О компании - Aliance Production
+                </h1>
+                <p class="header-text">
+Главное о компании
+                </p>
+                <div class="seporator"></div>
+ 
+
+
+
+        </div>
+        <!-- /.container -->
+    </header>
+
+<?php include_once('footer.php');?>
---
+++ public/js/main.js
@@ -0,0 +1,145 @@
+const navbar = document.querySelector(".navbar");
+const logoLight = document.querySelector(".logo-light");
+const logo = document.querySelector(".logo");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+const isFront = document.body.classList.contains("front-page");
+
+const lightModeOn = (event) => {
+    navbar.classList.add("navbar-light");
+}
+
+const lightModeOff = (event) => {
+    navbar.classList.remove("navbar-light");
+}
+
+const changeNavHeight = (height) => {
+    navbar.style.height = height;
+};
+
+const openMenu = (event) => { // функция открывания меню
+
+    menu.classList.add("is-open"); // вешает класс is-open
+    mMenuToggle.classList.add("close-menu");
+    document.body.style.overflow = "hidden"; // запрещаем прокрутку сайта
+    lightModeOn();
+
+};
+
+const closeMenu = (event) => { // функция закрывания меню
+    menu.classList.remove("is-open"); // убирает класс is-open
+    mMenuToggle.classList.remove("close-menu");
+    document.body.style.overflow = ""; // разрешает прокрутку сайта
+    lightModeOff();
+};
+
+window.addEventListener('scroll', () => {
+    this.scrollY > 1 ? changeNavHeight('4.5rem') : changeNavHeight('5.875rem');
+    if (isFront) {
+        this.scrollY > 1 ? lightModeOn() : lightModeOff();
+    }
+});
+
+mMenuToggle.addEventListener("click", (event) => {
+    event.preventDefault();
+    menu.classList.contains("is-open") ? closeMenu() : openMenu();
+});
+
+const swiperSteps = new Swiper(".steps-slider", {
+    speed: 400,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".steps-button-next",
+        prevEl: ".steps-button-prev",
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+
+        768: {
+            slidesPerView: 3,
+
+        },
+
+        1024: {
+            slidesPerView: 4,
+
+        },
+    },
+});
+
+const swiper = new Swiper('.features-slider', {
+    speed: 400,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".slider-button-next",
+        prevEl: ".slider-button-prev",
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+
+        768: {
+            slidesPerView: 3,
+
+        },
+
+        1024: {
+            slidesPerView: 4,
+
+        },
+        1200: {
+            slidesPerView: 5,
+
+        },
+    },
+});
+
+const swiperBlog = new Swiper(".blog-slider", {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".blog-button-next",
+        prevEl: ".blog-button-prev",
+    },
+
+});
+
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
+
+document.addEventListener("click", (event) => {
+    if (
+        event.target.dataset.toggle == "modal" ||
+        event.target.parentNode.dataset.toggle == "modal" ||
+        (!event.composedPath().includes(modalDialog)
+            && modal.classList.contains("is-open"))
+    ) {
+        event.preventDefault();
+        modal.classList.toggle("is-open");
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == "Escape" && modal.classList.contains("is-open")) {
+        modal.classList.toggle("is-open");
+    }
+});
+//const modal = document.querySelector(".modal");
+// const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+// const modalClose = document.querySelector(".modal-close");
+
+// modalToggle.forEach((element) => {
+//     element.addEventListener("click", (event) => {
+//         event.preventDefault();
+//         modal.classList.add("is-open");
+//     });
+// });
+
+// modalClose.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     modal.classList.remove("is-open");
+// });
