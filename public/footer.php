ef645412632ee8018c0ae75766f0c8e8aeb3d3c4
Artemis <69371778+fokstreet@users.noreply.github.com>
2026-08-29 20:36:20 +0300

    add:bot

--- public/footer.php
+++ public/footer.php
@@ -15,7 +15,10 @@
                             type="text" 
                             class="input"
                             name="username"  
-                            placeholder=" " />
+                            placeholder=" "
+                            maxlenght="100"
+                            required 
+                            />
                             <label class="input-group-label" for="user-name">Имя</label>
                         </div>
                         <!-- /.input-group -->
@@ -25,7 +28,10 @@
                             type="tel" 
                             class="input"
                             name="userphone" 
-                            placeholder=" " />
+                            placeholder=" "
+                            maxlenght="30"
+                            required 
+                            />
                             <label class="input-group-label" for="user-phone">Номер телефона</label>
                         </div>
                         <!-- /.input-group -->
--- public/handler.php
+++ public/handler.php
@@ -1,5 +1,26 @@
 <?php 
-$user_name = $_POST['username'];
-$user_phone = $_POST ['userphone'];
+$user_name = htmlspecialchars($_POST['username']);
+$user_phone = htmlspecialchars($_POST ['userphone']);
+
+$token = "8906225343:AAFFHZ_MPu8BQN3cX2o_yHsOuHGcGiZFryY";
+$chat_id = "-5200302439";
+
+$formData = array(
+    "Клиент: " => $user_name,
+    "Телефон: " => $user_phone
+);
+
+foreach($formData as $key => $value) {
+    $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A" ;
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
+
+if($sendToTelegram) {
+    echo "Success";
+} else {
+    echo "Error";
+}
+
 echo "Привет, " . $user_name . "<br>";
 echo "Ваш телефон: <b>" . $user_phone . "</b>";
